139c8338fe40608b193c397af56de0f437eeca3a
root <root@DESKTOP-LSDMKDN.localdomain>
2021-02-12 09:00:48 +0100

    functional_slot

---
+++ script.js
@@ -0,0 +1,67 @@
+console.log("ovonijetvojkompa-juter");
+//console.log($, "ourajt");
+
+//PRoBA RANDOM SLIKA NA UPLOAD
+var learyArray = new Array(
+  "leary1.bmp",
+  "lery2.bmp",
+  "leary3.bmp",
+  "leary4.bmp",
+  "leary5.bmp",
+  "leary6.bmp",
+  "leary7.bmp",
+  "leary8.bmp",
+  "leary9.bmp",
+  "leary10.bmp",
+  "leary11.bmp",
+  "leary12.bmp",
+  "leary13.bmp",
+  "leary14.bmp",
+  "leary15.bmp",
+  "leary16.bmp",
+  "leary17.bmp",
+  "leary18.bmp",
+  "leary19.bmp",
+  "leary20.bmp",
+  "leary21.bmp",
+  "leary22.bmp",
+  "leary23.bmp",
+  "leary24.bmp",
+  "leary25.bmp"
+);
+console.log(learyArray);
+window.onload = randomImg;
+
+var randomNum = Math.floor(Math.random() * learyArray.length);
+function randomImg() {
+  var randomNum = Math.floor(Math.random() * learyArray.length);
+  document.querySelectorAll(".slot__img img").src = learyArray[randomNum];
+}
+var slot = document.getElementsByClassName("slot")[0];
+
+slot.addEventListener("mousedown", function () {
+  event.stopPropagation();
+  slot.classList.add("rotate");
+  document.querySelector("img").classList.add("grow");
+  function ranDom() {
+    document.querySelector("img").src = learyArray[randomNum];
+  }
+  setTimeout(ranDom, 1000);
+  /////////////////////PROBA RANDOM ZA SVE
+  function randomAll() {
+    var imgz = document.querySelectorAll("img");
+    for (i = 0; i <= imgz.length; i++) {
+      /////ako je i=randomNum, nema fora efekta, ali baca random
+      imgz[i].src = learyArray[randomNum];
+    }
+  }
+  setTimeout(randomAll, 1000);
+  console.log("clix");
+});
+
+function otherImg() {
+  for (i = 0; i <= learyArray.length; i++) {
+    document.getElementsByClassName("slot__img")[i].src = learyArray[randomNum];
+  }
+  return otherImg();
+}
